a059ded7d3833b6059486fceab95734e10ed0786
muubaraq <sixthlogic@gmail.com>
2022-04-27 15:07:19 +0100

    html and css update

---
+++ index.js
@@ -0,0 +1,14 @@
+var coll = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
